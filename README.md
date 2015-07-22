96b3c474d944a2805e35795ba9133ee6b58d4606
Caleb Howington <howingtonc@gmail.com>
2015-07-22 16:37:24 -0400

    Added resonator functions to separate file, added README.md with instructions

---
+++ README.md
@@ -0,0 +1,26 @@
+# Data Analysis
+
+## Resonator Analysis
+To fit file.s2p, 
+```
+from ResonatorFit import plotS21, analyze
+
+results = analyze('path/to/file.s2p')
+
+```
+
+The object 'results' contains the fit parameters, data, and file information. 
+
+To plot file, 
+```
+plotS21('path/to/file.s2p')
+```
+
+## Qubit Analysis
+Coming
+
+## JPM Analysis
+Coming
+
+## Circuit Analysis
+Coming
--- ResonatorFit.py
+++ ResonatorFit.py
@@ -1,16 +1,17 @@
-from numpy import real, imag, pi, where, loadtxt, sqrt, diag
+from scipy.constants import c
 from scipy.optimize import curve_fit
-from bokeh.plotting import figure, output_notebook, show, gridplot
+from numpy import loadtxt, real, imag, pi, sqrt, diag, where
+from bokeh.plotting import *
 
-
-# Resonator Analysis
+output_notebook()
 
 def lorentz(x,*p):
 #     p = amp, f0, kappa
     return (real(p[0]/(1-1j*(2*pi*x - 2*pi*p[1])/(p[2]/2)))**2 + imag(p[0]/(1-1j*(2*pi*x - 2*pi*p[1])/(p[2]/2)))**2)**.5
 
 def analyze(filename):
-    fig1, x,y = plotS21(filename)
+    fig1, x,y = plotS21(filename, showPlot=False)
+    fig1.plot_width = 450
 #     Find Resonance
     f0 = x[where(y == y.max())][0]
     
@@ -22,7 +23,7 @@ def analyze(filename):
         width=450)
     fig.circle(x*1e-9,yvolt, fill_color=None)
     p, pcov = curve_fit(lorentz, x, yvolt, [yvolt.max(),f0,.01])
-
+    
     f0 = round(p[1]*1e-9, 6)
     kappa = p[2]
     q = p[1]/(2*pi*kappa)
@@ -40,10 +41,11 @@ def analyze(filename):
     print 'Q = {:.3f}+/- {:.3f} ({:.2}%)'.format(q, qerr, (qerr/q)*100)
 
     fig.line(x*1e-9,lorentz(x,*p))
+
     grid = gridplot([[fig1, fig]], title=filename)
     show(grid)
-
-    #     Create results dict
+    
+#     Create results dict
     results = {
         'filename': filename,
         'freq': x,
@@ -59,12 +61,14 @@ def analyze(filename):
     }
     return results
 
-def plotS21(filename):
+def plotS21(filename, showPlot=True):
     print(filename)
     x, y = loadtxt(filename, comments='!', skiprows=10, usecols=(0, 3), unpack=True)
     fig = figure(
         x_axis_label="Frequency (GHz)",
-        y_axis_label="|S21| (dBm)" ,
-        width=450)
+        y_axis_label="|S21| (dBm)")
     fig.circle(x*1e-9,y, fill_color=None)
-    return fig,x,y
+    if showPlot:
+        show(fig)
+    else:
+        return fig,x,y
